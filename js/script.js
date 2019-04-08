09cdace6afba54bbcb375a962645d1c093f810eb
cylchcoch <cylchcoch@gmail.com>
2019-04-08 11:10:49 +0100

    update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@
 function redirect(){
     window.location.href = "mailto:cylchcoch@gmail.com";
 }
+
+//facebook
+function facebook() {
+	window.location.href = "https://www.facebook.com/cylchcoch/";
+}
+
+//instagram
+function instagram() {
+	window.location.href = "https://www.instagram.com/cylchcoch/";
+}
